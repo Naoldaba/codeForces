5bfefd70d9dd3db8b3f3889c89401f9e81c765b6
NaolDaba <nahafile@gmail.com>
2025-03-15 15:36:51 +0300

    update variable names to meaningful ones

--- B_Even_Sum.py
+++ B_Even_Sum.py
@@ -1,20 +1,20 @@
-t =int(input())
+tests =int(input())
 
-for _ in range(t):
+for _ in range(tests):
     n = int(input())
-    a = list(map(int, input().split()))
+    array = list(map(int, input().split()))
 
-    tot = sum(a)
-    if tot%2 == 0:
+    total = sum(array)
+    if total%2 == 0:
         print(0)
         continue
 
-    res = float('inf')
-    for x in a:
-        temp = 0
+    min_operations = float('inf')
+    for x in array:
+        operations_count = 0
         while x%2 == (x//2) %2:
-            temp += 1
+            operations_count += 1
             x //= 2
-        res = min(res, temp+1)
+        min_operations = min(min_operations, operations_count+1)
     
-    print(res)
+    print(min_operations)
